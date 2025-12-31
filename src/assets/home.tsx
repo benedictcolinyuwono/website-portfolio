ce3f244145e4254bcb9386af9fb221107d8a816f
Benedict Colin Yuwono <colin2laguna@gmail.com>
2025-12-31 01:23:50 +0000

    Add files via upload

---
+++ src/assets/home.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+const Home: React.FC = () => {
+    return (
+        <main style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+            <div>
+                <div>
+                    <h1>Welcome to my Website!</h1>
+                    <p>
+                        This is the home page. Explore my features and learn more about what I offer!
+                    </p>
+                </div>
+            </div>
+
+        </main>
+    );
+};
+
+export default Home;
